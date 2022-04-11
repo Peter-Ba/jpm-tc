5ac30c2623972eda866e81cc84c57a63e8e0044c
Peter B <63689434+Peter-Ba@users.noreply.github.com>
2022-04-11 00:24:40 -0400

    created BE tests for getWeatherReport, getDistance and getWeather functions

--- backend/__tests__/getWeatherReport.test.js
+++ backend/__tests__/getWeatherReport.test.js
@@ -1,28 +1,40 @@
 const { getWeatherReport, getWeather, getDistance } = require('../getWeatherReport');
 
-describe('API call for weather report per city', () => {
-  test('a collection of cities', () => {
-    expect(getWeatherReport()).toEqual(1);
+const result = [
+  { "city": "Glasgow", "weather": "Cold" },
+  { "city": "London", "weather": "Chilly" },
+  { "city": "Brussels", "weather": "Chilly" },
+  { "city": "Tokyo", "weather": "Monsoon" },
+  { "city": "Honolulu", "weather": "Stormy" },
+  { "city": "Los-Angeles", "weather": "Sunny" }
+]
+
+describe('API GET response for weather report per city', () => {
+  const report = getWeatherReport();
+  const getWeather = getWeather();
+  const distance = getDistance(55.852059, 33.517941, -111.689978, -4.095055);
+
+  it('returns array with at least one object', () => {
+    expect(Array.isArray(report)).toBe(true);
+    expect(report.length > 0).toBe(true);
+    expect(report).toBe(result);
   });
-  // returns an array
-  // array is not empty
 
-  test('a collection of cities', () => {
-    expect(getWeather()).toEqual(1);
+  it('returns an object, with city and weather property', () => {
+    const keys = Object.keys(getWeather);
+    const regex = /Sunny|Stormy|Cold|Chilly|Monsoon/;
+
+    expect(typeof getWeather).toBe('object');
+    expect(!Array.isArray(getWeather)).toBe(true);
+    expect(keys.includes('city')).toBe(true);
+    expect(keys.includes('weather')).toBe(true);
+    expect(regex.test(keys)).toBe(true);
   });
 
-// returns an object
-// object is not empty
-// object has two properties, city and weather
-// only values allowed for weather are:
-// 'Sunny', 'Stormy', 'Cold', 'Chilly', 'Monsoon'
-
-
-  test('a collection of cities', () => {
-    expect(getDistance()).toEqual(1);
+  it('returns a calculated distance between lat/lon points', () => {
+    expect(typeof distance).toBe('number');
+    expect(distance).toEqual(10982.59452677817);
   });
-// input lat1, lon1, lat2, lon2
-// returns a decimal/integer
 
 });
 
